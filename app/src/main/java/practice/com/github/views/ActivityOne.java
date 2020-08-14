bdec5db582a8b8f91f2d697bb6071a76008539fc
Mahesh Balla <mahesh.balla461@gmail.com>
2020-08-14 20:29:00 +0530

    Response validation Added.

---
+++ app/src/main/java/practice/com/github/views/ActivityOne.java
@@ -0,0 +1,16 @@
+package practice.com.github.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import practice.com.github.R;
+
+public class ActivityOne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one);
+    }
+}
